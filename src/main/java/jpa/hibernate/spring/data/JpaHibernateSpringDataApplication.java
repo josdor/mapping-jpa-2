505f90da68cd38849f5245ef2b7c00176ba4a5c3
Razafinirina <Josdor.r@gmail.com>
2022-03-18 23:49:16 +0100

    first commit

---
+++ src/main/java/jpa/hibernate/spring/data/JpaHibernateSpringDataApplication.java
@@ -0,0 +1,13 @@
+package jpa.hibernate.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpa/hibernate/spring/data/JpaHibernateSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.hibernate.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
